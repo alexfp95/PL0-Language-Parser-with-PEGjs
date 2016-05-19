a2c5a27cae7c3059d7087e9f4a7b86bd5509770b
alu0100821338 <alu0100821338@ull.edu.es>
2016-05-19 11:38:12 +0000

    Insertando archivos base para proyecto. Daniel Daher. Cristina Tosco

---
+++ app.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+app.use(express.static(__dirname + '/public'));
+
+const calculate = require('./models/calculate');
+
+app.get('/', (request, response) => {     
+  response.render ('index', { title: "CSV"} );
+});
+
+app.get('/csv', (request, response) => {
+  response.send({ "rows": calculate(request.query.input) });
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`Node app is running at localhost: ${app.get('port')}` );
+});
