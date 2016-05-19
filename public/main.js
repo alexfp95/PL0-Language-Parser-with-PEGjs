3fc89cc91da66fa43b6a34ba4f4919d7f5866679
alu0100821338 <alu0100821338@ull.edu.es>
2016-05-19 14:44:05 +0000

    prueba

--- public/main.js
+++ public/main.js
@@ -1,11 +1,9 @@
 console.log('estoy en main');
-var semantic = require ("semantic");
 
 $(document).ready(function() {
   $('#parse').click(function() {
     try {
       var result = pl0.parse($('#input').val());
-      semantic(result);
       $('#output').html(JSON.stringify(result,undefined,2));
     } catch (e) {
       $('#output').html('<div class="error"><pre>\n' + JSON.stringify(e, null,4) + '\n</pre></div>');
