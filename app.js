f478e734d665a1ca50a88aa469ef4f40e81a756d
alu0100821338 <alu0100821338@ull.edu.es>
2016-05-19 12:27:30 +0000

    probando main

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ app.use(express.static(__dirname + '/public'));
 
 const principal = require('./public/main');
 
-app.get('/', (request, response) => {     
-  response.render ('index', { title: "CSV"} );
-});
-
 
 app.listen(app.get('port'), () => {
     console.log(`Node app is running at localhost: ${app.get('port')}` );
