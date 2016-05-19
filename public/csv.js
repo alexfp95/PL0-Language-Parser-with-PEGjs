f163fdae284d4a9417ebc70b439a99f58f81228e
alu0100821338 <alu0100821338@ull.edu.es>
2016-05-19 13:13:04 +0000

    probando main

--- public/csv.js
+++ public/csv.js
@@ -87,7 +87,7 @@ $(document).ready(() => {
 
       var result = pl0.parse($('#original').val());
       console.log(result);
-      $('#original').val = "ejemplo";
+      original.value = "ejemplo";
       $('#finaltable').html(JSON.stringify(result,undefined,2));
       console.log("fuera");
     } catch (e) {
