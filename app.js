a1347b058a5c243d9e0062a9ab43c14b9e2feb69
alu0100821338 <alu0100821338@ull.edu.es>
2016-05-21 18:15:24 +0000

    Arreglando.

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
-app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/lib'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/tests'));
 
 
